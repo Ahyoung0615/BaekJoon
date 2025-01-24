d6e9aef5fde3d5e32ab46d42d4d3a7bea3d8ce65
Ahyoung0615 <cat5495@naver.com>
2025-01-24 15:36:36 +0900

    feat : 프로그래머스 문제 풀이 - 2025.01.24

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,26 @@
 import java.util.Arrays;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        // 백준 아무거나
-        // Scanner scanner = new Scanner(System.in);/
+        // 백준 및 프로그래머스 아무거나
+        // Scanner scanner = new Scanner(System.in);
 
-        int[] numbers = {1, 2, 3, 4, 5};
+        int[] arr = {0, 1, 2, 3, 4};
+        int[][] queries = {{0, 3}, {1, 2}, {1, 4}};
 
         Solution solution = new Solution();
-        System.out.println(solution.solution(numbers));
+        System.out.println(Arrays.toString(solution.solution(arr, queries)));
     }
 }
 
 class Solution {
-    public int solution(int[] numbers) {
-        Arrays.sort(numbers);
-        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = arr;
+        for (int i = 0; i < queries.length; i++) {
+            
+        }
+        return answer;
     }
 }
-
-
